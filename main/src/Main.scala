ed49493910c07240e513c2ce1f5ef98278788058
Miraç Satıç <miracsa@yandex.com>
2015-02-28 23:12:14 +0200

    Initial commit

---
+++ main/src/Main.scala
@@ -0,0 +1,24 @@
+
+
+/**
+ * Created by mirac on 24.02.2015.
+ * deneme.
+ */
+object Main {
+
+  def Main(): Unit = {
+
+    var x = 0
+
+
+    for(x <- 1 until 20)
+    {
+
+      printf("Value of x: " + x)
+
+    }
+  }
+
+
+
+}
